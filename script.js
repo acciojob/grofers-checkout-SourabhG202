14466c5aaf00845a9a6827d4c017559935595aa7
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-21 11:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const priceElements=document.querySelectorAll(".price");
+	let total = 0;
+	priceElements.forEach(price =>{
+		total+=parseFloat(price.textContent);
+	});
+	const table = document.querySelector("table");
+	const newRow = document.createElement("tr");
+	const totalcell = document.querySelector("td");
+	totalcell.colSpan = 2;
+	totalcell.textContent = `Total Price: ₹${total}`;
+	totalcell.style.fontWeight = "bold";
+
+	newRow.appendChild(totalcell);
+	table.appendChild(newRow);
+
+	getSumBtn.disabled = true;
+
+
   
 };
 
